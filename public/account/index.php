ebff1de8629ca126e8086c8118b7d74a91d5f7c4
Dadojonov Khurshed Okhunjonovich <dadojonov.k_qschool@qsoft.ru>
2021-12-14 22:28:54 +0300

    ready

--- public/account/index.php
+++ public/account/index.php
@@ -1,11 +1,11 @@
 <?php
+session_start();
 if (! $_SESSION['auth']) {
     header('Location: /login/');
 } 
-
 use App\Models\User;
 require($_SERVER['DOCUMENT_ROOT'] . '/../vendor/autoload.php');
-session_start();
+
 $user = (new User)->select($_SESSION['auth']);
 ?>
 <!DOCTYPE html>
@@ -30,29 +30,11 @@ $user = (new User)->select($_SESSION['auth']);
                         </div>
                         <div class="col-sm-6 col-md-8">
                             <h4><?= $user['name'] ?></h4>
-                            <small><cite title="San Francisco, USA">San Francisco, USA <i class="glyphicon glyphicon-map-marker">
+                            <small><cite title="San Francisco, USA"><?= $user['country'] ?><i class="glyphicon glyphicon-map-marker">
                             </i></cite></small>
                             <p>
                                 <i class="glyphicon glyphicon-envelope"></i><?= $user['email'] ?>
-                                <br />
-                                <i class="glyphicon glyphicon-globe"></i><a href="http://www.jquery2dotnet.com">www.jquery2dotnet.com</a>
-                                <br />
-                                <i class="glyphicon glyphicon-gift"></i><?= $user['age'] ?></p>
-                            <!-- Split button -->
-                            <div class="btn-group">
-                                <button type="button" class="btn btn-primary">
-                                    Social</button>
-                                <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
-                                    <span class="caret"></span><span class="sr-only">Social</span>
-                                </button>
-                                <ul class="dropdown-menu" role="menu">
-                                    <li><a href="#">Twitter</a></li>
-                                    <li><a href="https://plus.google.com/+Jquery2dotnet/posts">Google +</a></li>
-                                    <li><a href="https://www.facebook.com/jquery2dotnet">Facebook</a></li>
-                                    <li class="divider"></li>
-                                    <li><a href="#">Github</a></li>
-                                </ul>
-                            </div>
+                                <br />                            
                         </div>
                     </div>
                 </div>
